f141de24a013c9b76741d8ed318884ce4bc1562d
Tree <tree@multiplexed.be>
2017-11-29 15:36:25 -0800

    okeedoke

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function sync(bpm, sampleRate){ // bpm, sampleRate,
 	this.t = 0
 	this.index = []
 	this.beatIndex = new Array()
+  this.beatz = {}
 }
 
 $.prototype.clearAll = function(bpm, samplerate){
@@ -33,6 +34,87 @@ $.prototype.tick = function(t, i){
 
 $.prototype.off = function(i){
 	this.index.splice(i,1,undefined)
+}
+
+$.prototype._update = function(i, fn){
+	this.index.splice(i,1,fn)
+}
+
+$.prototype.update = function(id, beats, fn){
+  console.log(id, beats, fn)
+  if(typeof id === 'number') {
+    fn = beats
+    beats = id
+    id = Date.now().toString()
+  }
+  if(true){ // RiP
+    var i = Math.ceil(this.spb * beats);
+    console.log('test')
+    var self = this;
+    var delta = 0
+    var skipNext = false
+    var skip = false 
+    var beat = 0
+    function swing(beat){
+      delta = (Math.floor(self.spb * beat))
+      skipNext = beat === 0 ? false : true
+    }
+    if(!self.beatz[id]){
+      var l = this.index.length;
+      var off = function(){
+        self.off(l)
+      };
+      var emit = new emitter()
+      emit.on('stop', off)
+      this.beatz[id] = {
+        fn: fn,
+        beats: beats,
+        emitter: emit,
+        index: l
+      } 
+      var pushFn  = createPushFn(id, beat, fn, beats, off)
+      this.index.push(pushFn)
+      console.log(this.beatz[id])
+      return emit
+    } else{
+      // test if beat is changed
+      var b = this.beatz[id]
+      if(!(b.beats === beats)){
+        var psh = createPushFn(id, beat, fn, i, beats, off)  
+        self._update(b.index, psh)
+        return b.emitter
+      }
+      else{
+        b[id].fn = fn
+        return b.emitter
+      }
+    }
+    
+
+    function createPushFn(id, fn, i, beats, off){
+    
+      return (function(fn, i, beats, off){
+        return function(t, a, f){
+          if(f % (i + delta) === 0) {
+            if(skip){
+              skip = false
+              return
+            }
+            if(skipNext){
+              skipNext = false
+              skip = true
+              if(delta >= i) {
+                skip = false
+              }
+            }
+            self.beatz[id].fn(t, ++beat, off, swing)
+            //fn.apply(fn, [t, ++beat, off, swing])
+            emit.emit('beat', beat)
+          }
+        }
+      })(fn, i, beats, off)
+    }
+  }
 }
 
 $.prototype.on = function(beats, fn){
